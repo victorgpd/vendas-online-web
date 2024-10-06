4424324e7673b0f2f40b2efe6eadf4705a3f5d3b
Victor <72409417+Victor007894@users.noreply.github.com>
2024-10-06 18:23:22 -0300

    aula-06

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import LoginScreen from "./modules/login"
+
+function App() {
+  return (
+    <>
+      <main>
+        <h1>Olá, Mundo!</h1>
+        <LoginScreen></LoginScreen>
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,13 @@
+import { BackgroundImage } from "../styles/LoginScreenStyles"
+
+function LoginScreen() {
+    return (
+        <>
+            <div>
+                <BackgroundImage src="./background.png" />
+            </div>
+        </>
+    )
+}
+
+export default LoginScreen
